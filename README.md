21efecdf0f0232a81e25d68f2960573f6663c50b
Victor Penso <vic.penso@gmail.com>
2012-01-27 15:29:03 +0100

    Plugin to monitor running queued suspended jobs

---
+++ README.md
@@ -0,0 +1,14 @@
+Description
+===========
+
+[Collectd][1] plugin to gather active jobs in [GridEngine][2].
+
+Copying
+=======
+
+Copyright 2011 Victor Penso  
+License [GPLv3][3] (see LICENSE file)
+
+[1]: http://collectd.org/
+[2]: http://gridscheduler.sourceforge.net/
+[3]: http://www.gnu.org/licenses/gpl-3.0.html
---
+++ gridengine-jobs.rb
@@ -0,0 +1,56 @@
+#!/usr/bin/env ruby
+
+#---------------------------------------------------------------
+##
+## GridEngine plugin for Collectd
+##
+## This is free software: you can redistribute it
+## and/or modify it under the terms of the GNU General Public
+## License as published by the Free Software Foundation,
+## either version 3 of the License, or (at your option) any
+## later version.
+##
+## This program is distributed in the hope that it will be
+## useful, but WITHOUT ANY WARRANTY; without even the implied
+## warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+## PURPOSE. See the GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public
+## License along with this program. If not, see
+##
+## <http://www.gnu.org/licenses/>.
+##
+##----------------------------------------------------------------
+## Author: Victor Penso
+## Copyright 2012
+## Version: 0.0.1
+##----------------------------------------------------------------
+
+hostname = ENV.has_key?('COLLECTD_HOSTNAME') ? ENV['COLLECTD_HOSTNAME'] : 'localhost'
+interval = ENV.has_key?('COLLECTD_INTERVAL') ? ENV['COLLECTD_INTERVAL'].to_i : 60
+
+command = %q[qstat -ext -u '*']
+
+while true
+
+  time=`date +%s`.chop
+  jobs = { :running => 0, :queued => 0, :suspended => 0  }
+
+  `#{command}`.split("\n")[2..-1].each do |job|
+    # get the job state
+    job.split[7].each_char do |flag|
+      case flag
+      when 'r': jobs[:running] += 1
+      when 'q': jobs[:queued] += 1
+      when /[sS]/: jobs[:suspended] += 1
+      end
+    end
+  end
+
+  puts %Q[PUTVAL #{hostname}/gridengine/gridengine_jobs interval=#{interval} #{time}:#{jobs[:running]}:#{jobs[:queued]}:#{jobs[:suspended]} ]
+  $stdout.flush # make sure to always write the output buffer before sleep
+
+  sleep interval
+
+end
+
